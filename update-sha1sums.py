cebbb82a3b2680d813b89dc07e5255fd3bcdada6
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 22:09:57 +0800

    RMX2185: update-sha1sums: Handle line with opts
    Change-Id: Ia41000b36f3535a3cf0ef8d7650d0d540de728de

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -16,8 +16,9 @@
 # limitations under the License.
 #
 
-from hashlib import sha1
+import os
 import sys
+from hashlib import sha1
 
 device='RMX1941'
 vendor='realme'
@@ -33,13 +34,11 @@ def cleanup():
         line = line[:-1]
 
         # Skip empty or commented lines
-        if len(line) == 0 or line[0] == '#':
+        if len(line) == 0 or line[0] == '#' or '|' not in line:
             continue
 
         # Drop SHA1 hash, if existing
-        if '|' in line:
-            line = line.split('|')[0]
-            lines[index] = '%s\n' % (line)
+        lines[index] = line.split('|')[0]
 
 
 def update():
@@ -59,16 +58,15 @@ def update():
         if needSHA1:
             # Remove existing SHA1 hash
             line = line.split('|')[0]
-            filePath = line.split(':')[1] if len(
-                line.split(':')) == 2 else line
 
+            filePath = line.split(';')[0].split(':')[-1]
             if filePath[0] == '-':
-                file = open('%s/%s' % (vendorPath, filePath[1:]), 'rb').read()
-            else:
-                file = open('%s/%s' % (vendorPath, filePath), 'rb').read()
+                filePath = filePath[1:]
 
-            hash = sha1(file).hexdigest()
-            lines[index] = '%s|%s\n' % (line, hash)
+            with open(os.path.join(vendorPath, filePath), 'rb') as f:
+                hash = sha1(f.read()).hexdigest()
+
+            lines[index] = '%s|%s' % (line, hash)
 
 
 if len(sys.argv) == 2 and sys.argv[1] == '-c':

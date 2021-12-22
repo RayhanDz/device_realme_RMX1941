19f6a0e1722fe809e08bd0a01771dbebbb937a0f
Fraschze97 <AIV-97@users.noreply.github.com>
2021-12-22 22:16:39 +0700

    RMX1941: nuke useless product stuff
    Signed-off-by: Fraschze97 <AIV-97@users.noreply.github.com>

--- nad_RMX1941.mk
+++ nad_RMX1941.mk
@@ -17,11 +17,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base_system.mk)
 
 # Inherit some common NusantaraROM stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)

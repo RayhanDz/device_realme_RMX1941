4791812bf5226d565d83fed9d0e821dcf4f13558
Fraschze97 <AIV-97@users.noreply.github.com>
2021-12-26 04:16:15 +0700

    RMX2185: zram : move zram.sh target copy out to system/bin
    Signed-off-by: Fraschze97 <AIV-97@users.noreply.github.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,8 +40,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := set_zram.sh
-LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/set_zram.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)

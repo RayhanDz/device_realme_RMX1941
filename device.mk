87f5ac0db2e88fc941eb0c8533197ec004241cf3
imranpopz <imranpopz@users.noreply.github.com>
2021-12-31 02:39:42 +0700

    RMX1941: Build RenderSript HIDL
    Signed-off-by: Fraschze97 <AIV-97@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -293,6 +293,10 @@ PRODUCT_PACKAGES += \
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \

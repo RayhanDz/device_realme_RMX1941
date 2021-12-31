c2d89b90d80535e43bf9be3887703de9cb2e3787
telepathine <telepathine@users.noreply.github.com>
2021-12-31 10:11:32 +0700

    RMX1941: build radio interface

--- device.mk
+++ device.mk
@@ -306,6 +306,20 @@ PRODUCT_COPY_FILES += \
 # NusantaraPapers
 PRODUCT_PACKAGES += \
     NusantaraPapers
+	
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0 \
+    android.hardware.broadcastradio@1.1 \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0
 
 # RcsService
 PRODUCT_PACKAGES += \

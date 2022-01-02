cd132853a3606db936affb8e41f7eda063b041d1
Volodymyr Zhdanov <wight554@gmail.com>
2022-01-02 14:06:15 +0530

    RMX1941: Disable configstore
    * it's finally deprecated in R
    * ship disable_configstore because we have API level 29
    
    Change-Id: Ia3c94eeec0dca120489290fe3d92927ac8127520

--- device.mk
+++ device.mk
@@ -105,7 +105,15 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor
-	
+
+# Dependencies of kpoc_charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
+# Disable Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \

42e43c8e4d650879c57b237e71793e888d993f62
AIV-97 <AIV-97@users.noreply.github.com>
2022-01-04 08:06:55 +0700

    RMX1941 : typo

--- device.mk
+++ device.mk
@@ -134,7 +134,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
-	
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

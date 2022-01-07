64e753e654a5c90f9b60daf5372c3623a21507d4
AIV-97 <AIV-97@users.noreply.github.com>
2022-01-07 08:49:21 +0700

    RMX1941: build Tinyxml.so

--- device.mk
+++ device.mk
@@ -418,6 +418,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
+	
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
+	
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \

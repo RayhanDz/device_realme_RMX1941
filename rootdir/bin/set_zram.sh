32f2c29e29ac256eb97a3f647254bce87d2d49c0
AIV-97 <AIV-97@users.noreply.github.com>
2022-03-05 05:55:06 +0700

    RMX1941: upfate zram config based from available ram
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>

--- rootdir/bin/set_zram.sh
+++ rootdir/bin/set_zram.sh
@@ -1,23 +1,42 @@
-#! /vendor/bin/sh
-
-# Setup ZRAM Based On Available Ram
+#!/system/bin/sh
+#
+#ifdef VENDOR_EDIT
+#huacai.zhou@PSW.BSP.kernel.drv, 2018/03/09, adjust zram size according to total ram size
 MemTotalStr=`cat /proc/meminfo | grep MemTotal`
 MemTotal=${MemTotalStr:16:8}
-echo 1 > /sys/block/zram0/reset
-echo 'zstd' > /sys/block/zram0/comp_algorithm
-sleep 0.5
-if [ $MemTotal -le 3145728 ]; then
-  #config 1.6GB zram size with memory less or equals to 3 GB
-  echo 1717986918 > /sys/block/zram0/disksize
+
+if [ $MemTotal -le 2097152 ]; then
+  #config 1GB zram size with memory less than 2 GB
+  echo zstd > /sys/block/zram0/comp_algorithm
+  echo 1342177280 > /sys/block/zram0/disksize
   echo 180 > /proc/sys/vm/swappiness
   echo 0 > /proc/sys/vm/direct_swappiness
-else
-  #config 2GB zram size with memory less or equals to 4 GB
+  echo 10 > /proc/sys/vm/watermark_scale_factor
+elif [ $MemTotal -le 3145728 ]; then
+  #config 1.6GB zram size with memory less than 3 GB
+  echo lz4 > /sys/block/zram0/comp_algorithm
+  echo 1717986918 > /sys/block/zram0/disksize
+  echo 160 > /proc/sys/vm/swappiness
+  echo 60 > /proc/sys/vm/direct_swappiness
+elif [ $MemTotal -le 4194304 ]; then
+  #config 2GB zram size with memory less than 4 GB
+  echo lz4 > /sys/block/zram0/comp_algorithm
   echo 2147483648 > /sys/block/zram0/disksize
   echo 160 > /proc/sys/vm/swappiness
   echo 60 > /proc/sys/vm/direct_swappiness
+elif [ $MemTotal -le 6291456 ]; then
+  #config 1GB zram size with memory 6 GB
+  echo lz4 > /sys/block/zram0/comp_algorithm
+  echo 1073741824 > /sys/block/zram0/disksize
+  echo 160 > /proc/sys/vm/swappiness
+  echo 60 > /proc/sys/vm/direct_swappiness
+else
+  #config 1GB zram size with memory greater than 6 GB
+  echo lz4 > /sys/block/zram0/comp_algorithm
+  echo 1073741824 > /sys/block/zram0/disksize
+  echo 160 > /proc/sys/vm/swappiness
+  echo 60 > /proc/sys/vm/direct_swappiness
 fi
-sleep 0.5
 mkswap /dev/block/zram0
-sleep 0.5
-swapon -d /dev/block/zram0
+swapon /dev/block/zram0
+#endif /* VENDOR_EDIT */

bc08b6519538c93a72fed774cd250ea1472fe2f9
AIV-97 <AIV-97@users.noreply.github.com>
2022-03-11 15:30:09 +0700

    RMX1941: drop gcam-go
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -124,15 +124,10 @@ PRODUCT_PACKAGES += \
 	
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0 \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1 \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2 \
-    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

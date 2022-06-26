1de3507e0592b0d52923579d35f76ccd341fc3aa
AIV-97 <fraschze97@nusantararom.org>
2022-06-26 01:34:30 +0000

    Kontol

---
+++ rr_RMX2185.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2020 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common NusantaraROM stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Inherit from RMX2185 device makefile
+$(call inherit-product, device/realme/RMX2185/device.mk)
+
+# NusantaraProject stuff
+TARGET_BOOT_ANIMATION_RES := 1560
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := nad_RMX2185
+PRODUCT_DEVICE := RMX2185
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme G35 Series
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2185 \
+    PRODUCT_NAME=RMX2185 \
+    PRIVATE_BUILD_DESC="full_oppo6762-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)

958cf49e599b7bb44ac569012c47828ec8cdca71
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-05 13:32:22 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_RMX2185.mk
+    $(LOCAL_DIR)/lineage_RMX2185.mk
 	
 COMMON_LUNCH_CHOICES := \
-        nad_RMX2185-eng \
-        nad_RMX2185-user \
-        nad_RMX2185-userdebug
+        lineage_RMX2185-eng \
+        lineage_RMX2185-user \
+        lineage_RMX2185-userdebug
